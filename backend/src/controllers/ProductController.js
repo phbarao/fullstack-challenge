a0ef009b9947f37ed30f939f1bf9d825bd5002ff
Pedro Barão <phbarao@gmail.com>
2021-01-28 18:24:44 -0300

    listing products by name

--- backend/src/controllers/ProductController.js
+++ backend/src/controllers/ProductController.js
@@ -2,7 +2,7 @@ import Product from '../models/Product.js';
 
 class ProductController {
   async index(req, res) {
-    const product = await Product.find().sort();
+    const product = await Product.find().sort({ title: 1 });
 
     res.json(product);
   }
